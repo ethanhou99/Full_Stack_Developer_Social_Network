9c913fee2fd8ecfa98fb4b4c8bd17a9f9c3b5b65
Yicun Hou <yicunhou@bu.edu>
2019-02-09 15:46:20 -0500

    Finished backend

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI:
-    'mongodb://ethan:Kevin0125@ds221115.mlab.com:21115/reactconnector'
+    'mongodb://ethan:Kevin0125@ds221115.mlab.com:21115/reactconnector',
+    secrectOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 //DB Config
 const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
 mongoose
-.connect(db)
+.connect(db, { useNewUrlParser: true })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!!'));
+//app.get('/', (req, res) => res.send('Hello World!!'));
+//passport middleware
+app.use(passport.initialize());
+
+//passport Config
+require('./config/passport')(passport);
 
 //Use routes
 app.use('/api/users', users);
